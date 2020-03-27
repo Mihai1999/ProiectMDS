8831a5e6c33802ca80bd84477a6c74049893e78a
Mihai1999 <38681125+Mihai1999@users.noreply.github.com>
2020-03-27 19:54:42 +0200

    Add files via upload

---
+++ Repositories/AbonamentRepository/IAbonamentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fitness.Models;
+
+namespace Fitness.Repositories.AbonamentRepository
+{
+    public interface IAbonamentRepository
+    {
+        List<Abonament> GetAll();
+        Abonament Get(int Id);
+        Abonament Create(Abonament Abonament);
+        Abonament Update(Abonament Abonament);
+        Abonament Delete(Abonament Abonament);
+    }
+}
